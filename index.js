4b759b2a8d6ecfc2124bd10191a18fd83af15659
Veronica Ulrikkeholm <108199135+veronisab@users.noreply.github.com>
2022-10-18 10:30:24 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+Challenge: 
+
+When the button is clicked, call out to the Bored API
+(URL: https://apis.scrimba.com/bored/api/activity)
+and replace the h4 with the activity from the API
+
+*/
+
+
+const button = document.getElementById('button')
+button.addEventListener('click', () => fetch('https://apis.scrimba.com/bored/api/activity')
+  .then(response => response.json())
+  .then(data => {      
+        document.getElementById('activity').textContent = data.activity}
+  ))
+
